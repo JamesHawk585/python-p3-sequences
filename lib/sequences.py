fe2a1ca3ee74976fd7abdf023c80e17f7584e3fb
James Hawk <jph94880@gmail.com>
2023-07-18 21:12:39 -0500

    Creates functioning fibonacci list. Passes 0 tests.

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
 
+# Fibonacci sequence
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+# 0 + 1 = 1
+# 1 + 1 = 2
+# 1 + 2 = 3
+# 2 + 3 = 5
+# 3 + 5 = 8
+# 5 + 8 = 13
+# 8 + 13 = 21
+# 13 + 21 = 34 
+
+# 1. Create list with first two fibonacci numbers
+# 2. Extend sum of last two numbers to list
+# 3. Repeat step 2 until list has reached the desired length
+
+
+
 def print_fibonacci(length):
-    pass
+    fibonacci_list = [0, 1]
+    if length <= 2:
+        return fibonacci_list[:length]
+    while len(fibonacci_list) < length:
+       next_num = fibonacci_list[-1] + fibonacci_list[-2]
+       fibonacci_list.append(next_num)
+    return fibonacci_list
+
+
+
+
+
